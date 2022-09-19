9a3a8d93f5603e40e903f213483d22b16e7d7ee0
95brsu <103598894+95brsu@users.noreply.github.com>
2022-09-19 12:32:47 +0300

    Update panel.sh

--- panel.sh
+++ panel.sh
@@ -24,6 +24,7 @@ options=(
 "KNSTLD статус"
 "STRIDE логи"
 "STRIDE статус"
+"APTOS логи"
 "APTOS метрики"
 "APTOS ПЕРЕЗАПУСК(Аккуратно!!!)"
 "Выход")
@@ -108,14 +109,22 @@ curl localhost:16657/status
 break
 ;;
 
-"APTOS метрики"
+"APTOS логи")
 
 cd .aptos && curl 127.0.0.1:9101/metrics 2> /dev/null | grep "aptos_state_sync_version"
 
 break
 ;;
 
-"APTOS ПЕРЕЗАПУСК(Аккуратно!!!)"
+
+"APTOS метрики")
+
+cd .aptos && curl 127.0.0.1:9101/metrics 2> /dev/null | grep "aptos_state_sync_version"
+
+break
+;;
+
+"APTOS ПЕРЕЗАПУСК(Аккуратно!!!)")
 
 cd .aptos && docker-compose stop && docker-compose up -d
 
